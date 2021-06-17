2fc99a073e7c447e0c3991d2af87f51d4690356f
pritesh6754 <86052547+pritesh6754@users.noreply.github.com>
2021-06-17 16:01:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<HTML>
+<head>
+	 
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="format-detection" content="telephone=no">
+      <link rel="stylesheet" href="">
+      <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat|Cookie&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Delius&display=swap" rel="stylesheet">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" crossorigin="anonymous">
+	<center><title>Citizens Bank</title></center>
+	<link rel = "stylesheet" type = "text/css" href = "buttons.css">
+</head>
+
+<body style="background-image:url(bankbg.png);background-attachment: fixed;background-repeat: no-repeat;background-size: cover;">
+	
+    
+
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+
+
+	<div class="one" style=" background-color:rgb(112, 243, 177); opacity:0.9;height:40px;border-radius:8px; width:90%;margin:auto; border:2px solid black;padding:10px">
+		<table width="1330">
+            		
+			<table width="1200" align="center"  class = "t">
+                <tr>
+				<td style = "text-align:center"> <button class = "btn1" style="color:black ;border-radius:5px;"> Home </button></td> 	
+				<td style = "text-align:center"><a href="customer.php" target="frame"><button class = "btn2" style="color:black;border-radius:5px;">View Customers</button></a></td>
+				<td style = "text-align:center"><a href="Transfermoney.php" target="frame"><button class = "btn2" style="color:black;border-radius:5px;">Transfer Money</button></a></td>
+				<td style = "text-align:center"><a href="transactionhistory.php" target="frame"><button class = "btn2" style="color:black;border-radius:5px;">View Transaction History</button></a></td>
+                </tr>
+            </table>
+        </table>
+	</div>
+
+<br>
+<br>	
+<h1 >
+<span style="color: #47db1a;">C</span>
+<span style="color: #47db1a;">I</span>
+<span style="color: #47db1a;">T</span>
+<span style="color: #47db1a;">I</span>
+<span style="color: #47db1a;">Z</span>
+<span style="color: #47db1a;">E</span>
+<span style="color: #47db1a;">N</span>
+<span style="color: #47db1a;">S</span>
+
+<span style="color: #008cff;">B</span>
+<span style="color: #008cff;">A</span>
+<span style="color: #008cff;">N</span>
+<span style="color: #008cff;">K</span>
+</h1>
+    <br>
+    <br>
+	<div class="three" >This is the Basic Banking System that helps you to transfer amount of money between users .<br> You just need to click on Customer's name and it will automatically jump to next page to transfer the amount<br> where you need to select the user to whom you want to transfer money and the amount you want to transfer and click on submit button.Congratulations your transaction will be successfull!<br>
+	 
+</div>
+<br>
+<br>
+<div class="five">Designed & Developed By Pritesh Jadhav |  THE SPARKS FOUNDATION internship.<br>#GRIPJUNE2021<br> jadhavpritesh98@gmail.com</div>
+        
+</body>
+</html>
